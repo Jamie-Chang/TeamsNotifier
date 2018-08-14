d8aae1210c45928c25858a04f25436eb75bdab67
Jamie Chang <jamie.chang@playtech.com>
2018-08-14 15:14:52 +0100

    Correctly pushes notifications on chat and group chat messages

--- background.js
+++ background.js
@@ -3,8 +3,54 @@
 'use strict';
 
 
-function doStuff(details) {
-  console.log(details);
+function process_chat_link(link) {
+  link = link.replace('db3pv2.ng.msg.', '')
+  link = link.replace('v1/users/ME', '_#')
+  link += '?ctx=chat'
+  return link
+}
+
+
+function handle_message(event_message) {
+  if (event_message.resourceType && event_message.resourceType == 'NewMessage') {
+    let resource = event_message.resource
+    if (resource.type == 'Message' &&
+      resource.threadtype == 'chat' &&
+      resource.messagetype == 'Text'
+    ){
+      console.log(resource.imdisplayname);
+      console.log(resource.content);
+      browser.notifications.create(process_chat_link(resource.conversationLink), {
+        "type": "basic",
+        "iconUrl": browser.extension.getURL("images/teams256.png"),
+        "title": resource.imdisplayname + (resource.threadtopic?' in ' + resource.threadtopic: ''),
+        "message": resource.content
+      })
+    }
+  }
+  // TODO: Handle ConversationUpdate resourceType
+}
+
+
+function onPollRequest(details) {
+  console.log(details)
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder()
+  filter.ondata = function(event) {
+    try {
+      let data = JSON.parse(decoder.decode(event.data, {stream: true}))
+      if (data.eventMessages) {
+        for (let i = 0; i < data.eventMessages.length; i++) {
+          handle_message(data.eventMessages[i]);
+        }
+      }
+    }
+    finally {
+      filter.write(event.data);
+      filter.disconnect();
+    }
+  }
 }
 
 var teams = {
@@ -13,14 +59,19 @@ var teams = {
   resetTab: function() {
     this.tab = null;
     browser.browserAction.setIcon({path: 'images/teamsgrey38.png'});
-    browser.webRequest.onBeforeRequest.removeListener(doStuff);
+    browser.webRequest.onBeforeRequest.removeListener(onPollRequest);
   },
   setTab: function(tab) {
     this.tab = tab;
     browser.browserAction.setIcon({path: 'images/teams38.png'});
+    console.log("Setting tab")
     browser.webRequest.onBeforeRequest.addListener(
-      doStuff,
-      {tabId: this.tab.id, urls: ['https://emea.ng.msg.teams.microsoft.com/v1/users/ME/endpoints/SELF/subscriptions/0/poll']}
+      onPollRequest,
+      {
+        tabId: this.tab.id,
+        urls: ['https://emea.ng.msg.teams.microsoft.com/v1/users/ME/endpoints/SELF/subscriptions/0/poll']
+      },
+      ['blocking']
     )
   }
 }
@@ -43,12 +94,17 @@ function findTeams() {
 
 function goToTeams() {
   if (teams.tab !== null) {
-    browser.tabs.update(teams.tab.id, {highlighted: true});
+    browser.tabs.update(teams.tab.id, {active: true});
     return;
   }
   findTeams();
   if (teams.tab === null) browser.tabs.create({url: teams.url}, function(tab) {teams.setTab(tab);});
-  else browser.tabs.update(teams.tab.id, {highlighted: true});
+  else browser.tabs.update(teams.tab.id, {active: true});
+}
+
+function goToTeamsChat(link) {
+  goToTeams();
+  browser.tabs.update(teams.tab.id, {url: link})
 }
 
 function createdCallback(tab) {
@@ -80,5 +136,8 @@ browser.runtime.onInstalled.addListener(function() {
   browser.tabs.onRemoved.addListener(removedCallback);
   browser.tabs.onUpdated.addListener(updatedCallback);
 });
-browser.browserAction.onClicked.addListener(function(tab) {goToTeams();});
 findTeams();
+browser.notifications.onClicked.addListener(function(notification_id) {
+  browser.notifications.clear(notification_id);
+  goToTeamsChat(notification_id);
+});
