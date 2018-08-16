38b05b91faaa1b174fe1d166da43603e8fc704d2
JChangg <cc751@gmail.com>
2018-08-16 01:22:22 +0100

    Shows noticiations for channels

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A Firefox extension to send notifications for new messages in MS Teams
 TODO:
 * ~~Focus window when clicking on notification or icon.~~
 * ~~Handle larger messages properly.~~
-* Show notifications for channels.
+* ~~Show notifications for channels.~~
--- background.js
+++ background.js
@@ -1,32 +1,44 @@
 /*global browser*/
+/*jslint es6 */
 
 'use strict';
 
 
 function process_chat_link(link) {
-  link = link.replace('db3pv2.ng.msg.', '')
-  link = link.replace('v1/users/ME', '_#')
-  link += '?ctx=chat'
-  return link
+  link = link.replace('db3pv2.ng.msg.', '');
+  link = link.replace('v1/users/ME', '_#');
+  link += '?ctx=chat';
+  return link;
+}
+
+function process_topic_link(link, threadtopic, messageId) {
+  link = link.replace('db3pv2.ng.msg.', '');
+  link = link.replace('v1/users/ME', '_#');
+  link = link.replace('_#/conversations/', '_#/conversations/' + threadtopic + '?threadId=');
+  link += '&ctx=channel';
+  link += ('&messageId=' + messageId);
+  return link;
 }
 
 
 function handle_message(event_message) {
   if (event_message.resourceType && event_message.resourceType == 'NewMessage') {
-    let resource = event_message.resource
-    if (resource.type == 'Message' &&
-      resource.threadtype == 'chat' &&
-      resource.messagetype == 'Text'
-    ){
-      browser.notifications.create(process_chat_link(resource.conversationLink), {
-        "type": "basic",
-        "iconUrl": browser.extension.getURL("images/teams256.png"),
-        "title": resource.imdisplayname + (!resource.threadtopic.includes(':orgid:')?' in ' + resource.threadtopic: ''),
-        "message": resource.messagetype == 'Text'?resource.content:'...'
-      })
+    let resource = event_message.resource;
+    if (resource.type != 'Message') return;
+    let link = null;
+    if (resource.threadtype == 'chat') link = process_chat_link(resource.conversationLink);
+    else if (resource.threadtype == 'topic') {
+      link = process_topic_link(
+        resource.conversationLink, resource.threadtopic, resource.id);
     }
+    else return;
+    browser.notifications.create(link, {
+      type: "basic",
+      iconUrl: browser.extension.getURL("images/teams256.png"),
+      title: resource.imdisplayname + (!resource.threadtopic.includes(':orgid:')?' in ' + resource.threadtopic: ''),
+      message: resource.messagetype == 'Text'?resource.content:'New ' + resource.messagetype + ' message'
+    });
   }
-  // TODO: Handle ConversationUpdate resourceType
 }
 
 
@@ -87,14 +99,16 @@ var teams = {
 }
 
 
-function findTeams() {
+function findTeams(skipTabId) {
+  skipTabId =  (arguments.length == 1?skipTabId:null)
   let queryInfo = {url: [teams.url + '/*']};
   browser.tabs.query(queryInfo, function(tabs) {
     console.log(tabs)
     for (let i = 0; i < tabs.length; i++) {
-      let tab = tabs[i];
-      teams.setTab(tab);
-      return;
+      if (tabs[i].id != skipTabId) {
+        teams.setTab(tabs[i]);
+        return;
+      }
     }
   });
   if (teams.tab === null) teams.resetTab();
@@ -112,8 +126,8 @@ function goToTeams() {
 }
 
 function goToTeamsChat(link) {
-  goToTeams();
   browser.tabs.update(teams.tab.id, {url: link})
+  goToTeams();
 }
 
 function createdCallback(tab) {
@@ -124,18 +138,22 @@ function createdCallback(tab) {
 
 
 function updatedCallback(tab_id, change_info, tab) {
-  if (!change_info.url) return;
-  if (teams.tab!== null && tab.id === teams.tab.id && !change_info.url.startsWith(teams.url)) {
+  if (teams.tab !== null && tab.id === teams.tab.id) {
     // Teams tab changed URL
-    teams.resetTab();
-  }
-  else createdCallback(tab);
+    teams.tab = tab;
+    if (change_info.url && !change_info.url.startsWith(teams.url)) {
+      // Teams tab URL changed
+      teams.resetTab();
+      findTeams();
+    }
+  } else createdCallback(tab);
 }
 
 
 function removedCallback(tab_id, remove_info) {
   if (teams.tab !== null && teams.tab.id == tab_id) {
     teams.resetTab();
+    findTeams(tab_id);
   }
 }
 
@@ -144,8 +162,8 @@ browser.runtime.onInstalled.addListener(function() {
   browser.tabs.onCreated.addListener(createdCallback);
   browser.tabs.onRemoved.addListener(removedCallback);
   browser.tabs.onUpdated.addListener(updatedCallback);
+  findTeams();
 });
-findTeams();
 browser.notifications.onClicked.addListener(function(notification_id) {
   browser.notifications.clear(notification_id);
   goToTeamsChat(notification_id);
